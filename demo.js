c89a4d4ad1226e41fa87e9efae00ba54d8f27e6a
chonpin <chonpin@gmail.com>
2018-05-03 22:48:43 +0800

    adjust index.js to demo.js

---
+++ demo.js
@@ -0,0 +1,21 @@
+/**
+	NodeJS code challenge assignment
+	Author : chonpin
+	CreateDate : 2018/05/02
+*/
+
+
+var fs = require('fs');
+var Log = require('./log.js')
+var config = require('./config.js');
+var Detect = require('./detect.js')
+
+log = new Log();
+log.setDebugMode(config.debug);
+log.setLogLevel(config.logLevel);
+
+var data = fs.readFileSync(config.documentURI).toString();
+log.debug(data);
+
+detect = new Detect(data);
+detect.checkTitle().checkDescriptions().checkKeywords().getH1Count().getStrongCount();
