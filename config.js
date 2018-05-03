93fdc46886ccbf700d9254390cea4ca75edfd7c9
chonpin <chonpin@gmail.com>
2018-05-03 22:01:34 +0800

    adjust code for spec

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 var config = {
 	debug : true,
 	logLevel : 'info',	// log level (debug -> info -> error)
-	documentURI : './resource/nodejs.html'	// Detect resource 
+
+	documentURI : './resource/nodejs.html',	// Detect resource 
+	maxStrongTagCount : 15 
 };
 
 module.exports = config;
--- detect.js
+++ detect.js
@@ -5,55 +5,46 @@
 */
 
 function Detect(data) {
-	var cheerio = require('cheerio');	// https://github.com/cheeriojs/cheerio
+	var config = require('./config.js');
+	var cheerio = require('cheerio');	// Fast, flexible, and lean implementation of core jQuery designed specifically for the server.  [ https://cheerio.js.org/ ]
 	var $ = cheerio.load(data.toLowerCase());
 
 	this.checkTitle = function(){
 		var result = $('head title').html();
-		if (result!==null){
-			console.log("Find <title> tag");
-		}else{
-			console.log("No <title> tag");
+		if (result===null){
+			console.log("<title> Not Found");
 		}
 		return this;
 	}
 
 	this.checkDescriptions = function(){
 		var result = $('head meta[name=descriptions]').html();
-		if (result!==null){
-			console.log("Find descriptions meta tag");
-		}else{
-			console.log("No descriptions meta tag");
+		if (result===null){
+			console.log("descriptions meta Not Found");
 		}
 		return this;
 	}
 
 	this.checkKeywords = function(){
 		var result = $('head meta[name=keywords]').html();
-		if (result!==null){
-			console.log("Find keywords meta tag");
-		}else{
-			console.log("No keywords meta tag");
+		if (result===null){
+			console.log("keywords meta Not Found");
 		}
 		return this;
 	}
 
 	this.getH1Count = function(){
 		var result = $('h1').length;
-		if (result>0){
-			console.log("Find "+result+" <H1> tag");
-		}else{
-			console.log("<H1> tag not found");
+		if (result>1){
+			console.log("More than one <H1> tag");
 		}
 		return this;
 	}
 
 	this.getStrongCount = function(){
 		var result = $('strong').length;
-		if (result>0){
-			console.log("Find "+result+" <strong> tag");
-		}else{
-			console.log("<strong> tag not found");
+		if (result>config.maxStrongTagCount){
+			console.log("Too More <strong> tag ("+result+"/"+config.maxStrongTagCount+")");
 		}
 		return this;
 	}
